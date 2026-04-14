f7810ae36606321a98470fdf83e483b3f4cf7e74
Afzal Hossan <afzalhossan2005021@gmail.com>
2026-04-14 11:40:12 +0600

    New features added to import.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,19 @@
 from .src import (
     pairwise_align,
     hierarchical_pairwise_align,
+    align_multiple_slices,
     calculate_performance_metrics,
     calculate_forward_reverse_compactness,
-    visualize_alignment
+    visualize_alignment,
+    visualize_3d_stack
 )
 
 __all__ = [
     'pairwise_align',
     'hierarchical_pairwise_align',
+    'align_multiple_slices',
     'calculate_performance_metrics',
     'calculate_forward_reverse_compactness',
-    'visualize_alignment'
+    'visualize_alignment',
+    'visualize_3d_stack'
 ]
